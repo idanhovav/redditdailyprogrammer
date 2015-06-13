fa6f7c90403058829b83eadd0c4d5227568c5f22
Idan <idanhovav@users.noreply.github.com>
2015-06-13 13:03:36 -0700

    second challenge

---
+++ easy2.py
@@ -0,0 +1,16 @@
+import math
+
+def add(a, b):
+	return a+b
+
+def mul(a,b):
+	return a*b
+
+def mod(a, b):
+	return a % b
+
+#given two sides of a right triangle,
+#this function returns the hypotenuse length
+def hypotenuse(a,b):
+	return math.sqrt( a**2 + b**2 )
+
